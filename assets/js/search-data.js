8758d9c97e30aab722f38fb15e5266832b920e98
bhundiya <46730108+bhundiya@users.noreply.github.com>
2025-11-08 00:08:25 +0000

    Deploying to gh-pages from @ bhundiya/bhundiya.github.io@6045fba84bc92443828460bdf417238f78fc61af 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -408,51 +408,21 @@ ninja.data = [{
             },},{id: "news-the-sparc-lab-website-is-live",
           title: 'The SPARC lab website is live!',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-spacecraft-attitude-control",
+          title: 'Spacecraft Attitude Control',
+          description: "",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+              window.location.href = "/projects/attitude_control/";
+            },},{id: "projects-efficient-mission-design",
+          title: 'Efficient Mission Design',
+          description: "",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+              window.location.href = "/projects/mission_design/";
+            },},{id: "projects-low-power-robotic-construction",
+          title: 'Low-power Robotic Construction',
+          description: "",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
+              window.location.href = "/projects/robotic_construction/";
             },},{
         id: 'social-email',
         title: 'email',
